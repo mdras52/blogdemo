1883557f58d4def20bd62b3b7293d9b5e4765432
Matthew Rasler <mdras52@Matthews-MacBook-Pro.local>
2021-02-16 13:51:30 -0700

    removing unneeded setup script as it has all been moved to the user data script

--- blog_setup.sh
+++
@@ -1,8 +0,0 @@
-# create directory for site content
-sudo mkdir -p /var/www/yoursitename
-sudo chown ghostuser:ghostuser /var/www/yoursitename
-sudo chmod 775 /var/www/yoursitename
-
-sudo su - ghostuser
-cd /var/www/yoursitename
-ghost
